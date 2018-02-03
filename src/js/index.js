9f9a9dd1c54681f84a9f551bc036cba29830659a
Duchamp Mavrick <clownclowed@gmail.com>
2018-02-03 03:29:33 +0100

    Create map

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,49 @@
-var game = new Phaser.Game(500, 500, Phaser.AUTO);
+var sizeTiles = 50;
+var game = new Phaser.Game(sizeTiles * 5, sizeTiles * 10, Phaser.AUTO);
 
 var gameState = {
-	preload: function() {},
-	create: function() {},
+	preload: function() {
+		game.load.image('whiteSquarre', '/src/assets/white_squarre.jpg');
+		game.load.image('redSquarre', '/src/assets/red_squarre.png');
+	},
+	create: function() {
+		this.tileWidth = '100px';
+		this.tileHeight = '100px';
+
+		this.tileGrid = [
+			[1, 0, 1, 0, 1],
+			[0, 0, 0, 1, 0],
+			[0, 0, 0, 1, 0],
+			[0, 1, 0, 1, 0],
+			[1, 0, 1, 0, 0],
+			[1, 0, 0, 0, 1],
+			[0, 1, 0, 0, 0],
+			[0, 1, 0, 0, 0],
+			[0, 0, 1, 0, 0],
+			[0, 0, 0, 0, 0],
+		];
+		this.initGrid(this.tileGrid);
+	},
 	update: function() {},
+	initGrid: function(tileGrid) {
+		for (let i = 0; i < tileGrid.length; i++) {
+			for (let j = 0; j < tileGrid[i].length; j++) {
+				var image;
+				switch (tileGrid[i][j]) {
+					case 1:
+						image = 'redSquarre';
+						break;
+
+					default:
+						image = 'whiteSquarre';
+						break;
+				}
+				var myImage = game.add.sprite(sizeTiles * j, sizeTiles * i, image);
+				myImage.width = sizeTiles;
+				myImage.height = sizeTiles;
+			}
+		}
+	},
 };
 
 game.state.add('gameState', gameState);
