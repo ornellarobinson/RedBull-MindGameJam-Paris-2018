843adfaf17d57c9be78fe56561228c893db54fca
Duchamp Mavrick <clownclowed@gmail.com>
2018-02-04 12:38:26 +0100

    rotate air

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,26 @@ var changeStartArrowDirection;
 var arrowStart;
 var myImage = [];
 var arrowDirection;
+var positionLine = [25, 475];
+var lineTab = 9;
+var columnTab = 0;
+var lineDirection = 'up';
+var gameOver = false;
 var game = new Phaser.Game(sizeTiles * 5, sizeTiles * 11, Phaser.AUTO);
+
+var fire = {
+	id: 4,
+	direction: 'up',
+};
+
+var vent = {
+	id: 5,
+	direction: 'right',
+};
+
+var toggle = {
+	actualSprite: 'fire',
+};
 
 var gameState = {
 	preload: function() {
@@ -24,7 +43,7 @@ var gameState = {
 		this.tileGrid = [
 			[1, 0, 4, 0, 3],
 			[0, 0, 0, 1, 0],
-			[0, 0, 0, 1, 2],
+			[0, 0, 0, 1, 1],
 			[0, 2, 0, 1, 0],
 			[1, 1, 0, 0, 0],
 			[4, 0, 0, 0, 2],
@@ -55,34 +74,11 @@ var gameState = {
 		);
 		changeStartArrowDirection.anchor.setTo(0.5, 0.5);
 		changeStartArrowDirection.inputEnabled = true;
-		// arrowDirection.angle = 0;
-		//Création des 4 éléments
-		var air = game.add.sprite(75, 505, 'air');
-		air.width = 40;
-		air.height = 40;
-		game.physics.arcade.enable(air);
-		// air.anchor.x = 0.5;
-		air.inputEnabled = true;
-		air.input.enableDrag(true);
-		air.originalPosition = air.position.clone();
-		air.events.onDragStop.add(function(currentSprite, pointer) {
-			var line = Math.floor(currentSprite.position.y / 50);
-
-			var column = Math.floor(currentSprite.position.x / 50);
-
-			if (this.tileGrid[line][column] === 0)
-				currentSprite.position.copyFrom({
-					y: line * 50 + 10,
-					x: column * 50 + 25,
-				});
-			else currentSprite.position.copyFrom(currentSprite.originalPosition);
-		}, this);
 
 		fire = this.add.sprite(105, 505, 'fire');
 		fire.width = 40;
 		fire.height = 40;
 		game.physics.arcade.enable(fire);
-		air.anchor.x = 0.5;
 		fire.inputEnabled = true;
 		fire.input.enableDrag(true);
 		fire.originalPosition = fire.position.clone();
@@ -91,12 +87,38 @@ var gameState = {
 
 			var column = Math.floor(currentSprite.position.x / 50);
 
-			if (this.tileGrid[line][column] === 0)
+			if (this.tileGrid[line][column] === 0) {
 				currentSprite.position.copyFrom({
 					y: line * 50 + 10,
 					x: column * 50 + 5,
 				});
-			else currentSprite.position.copyFrom(currentSprite.originalPosition);
+				this.tileGrid[line][column] = 5;
+			} else currentSprite.position.copyFrom(currentSprite.originalPosition);
+		}, this);
+
+		this.air = game.add.sprite(75, 525, 'air');
+		this.air.width = 40;
+		this.air.height = 40;
+		this.air.angle = 0;
+		game.physics.arcade.enable(this.air);
+		this.air.inputEnabled = true;
+		this.air.anchor.setTo(0.5);
+		this.air.input.enableDrag(true);
+		this.air.originalPosition = this.air.position.clone();
+		this.air.events.onDragStop.add(function(currentSprite, pointer) {
+			var line = Math.floor(currentSprite.position.y / 50);
+			var column = Math.floor(currentSprite.position.x / 50);
+
+			if (this.tileGrid[line][column] === 0) {
+				currentSprite.position.copyFrom({
+					y: line * 50 + 25,
+					x: column * 50 + 25,
+				});
+				this.tileGrid[line][column] = 6;
+			} else {
+				currentSprite.position.copyFrom(currentSprite.originalPosition);
+				if (this.tileGrid[line][column] >= 5) this.tileGrid[line][column] = 0;
+			}
 		}, this);
 
 		water = this.add.sprite(175, 505, 'water');
@@ -111,12 +133,13 @@ var gameState = {
 
 			var column = Math.floor(currentSprite.position.x / 50);
 
-			if (this.tileGrid[line][column] === 0)
+			if (this.tileGrid[line][column] === 0) {
 				currentSprite.position.copyFrom({
 					y: line * 50 + 5,
 					x: column * 50 + 25,
 				});
-			else currentSprite.position.copyFrom(currentSprite.originalPosition);
+				this.tileGrid[line][column] = 7;
+			} else currentSprite.position.copyFrom(currentSprite.originalPosition);
 		}, this);
 
 		earth = this.add.sprite(225, 505, 'earth');
@@ -131,24 +154,152 @@ var gameState = {
 
 			var column = Math.floor(currentSprite.position.x / 50);
 
-			if (this.tileGrid[line][column] === 0)
+			if (this.tileGrid[line][column] === 0) {
 				currentSprite.position.copyFrom({
 					y: line * 50 + 5,
 					x: column * 50 + 25,
 				});
-			else currentSprite.position.copyFrom(currentSprite.originalPosition);
+				this.tileGrid[line][column] = 8;
+			} else currentSprite.position.copyFrom(currentSprite.originalPosition);
 		}, this);
+
+		this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
+		this.a = game.input.keyboard.addKey(Phaser.Keyboard.A);
+		this.a.onDown.add(this.rotateAir, this);
+
+		game.time.events.repeat(
+			Phaser.Timer.SECOND * 1,
+			1000,
+			this.rotateToggle,
+			this,
+		);
 	},
 	alternatechangeStartArrowDirection: function() {
 		if (changeStartArrowDirection.angle === 0) {
 			changeStartArrowDirection.angle = 90;
 			arrowStart.angle = 0;
+			lineDirection = 'up';
 		} else {
 			changeStartArrowDirection.angle = 0;
 			arrowStart.angle = 90;
+			lineDirection = 'right';
 		}
 	},
-	update: function() {},
+	rotateToggle: function() {
+		if (gameOver) return;
+		if (myImage[2].key === 'fire') {
+			var x = myImage[2].position.x;
+			var y = myImage[2].position.y;
+			myImage[2] = game.add.sprite(x, y, 'air');
+			myImage[2].height = sizeTiles;
+			myImage[2].width = sizeTiles;
+			this.tileGrid[0][2] = 6;
+
+			var x = myImage[25].position.x;
+			var y = myImage[25].position.y;
+			myImage[25] = game.add.sprite(x, y, 'air');
+			myImage[25].height = sizeTiles;
+			myImage[25].width = sizeTiles;
+			this.tileGrid[5][0] = 6;
+
+			var x = myImage[44].position.x;
+			var y = myImage[44].position.y;
+			a = game.add.sprite(x, y, 'air');
+			a.height = sizeTiles;
+			a.width = sizeTiles;
+			this.tileGrid[8][4] = 6;
+		} else if (myImage[2].key === 'air') {
+			var x = myImage[2].position.x;
+			var y = myImage[2].position.y;
+			myImage[2] = game.add.sprite(x, y, 'water');
+			myImage[2].height = sizeTiles;
+			myImage[2].width = sizeTiles;
+			this.tileGrid[0][2] = 7;
+			var x = myImage[25].position.x;
+			var y = myImage[25].position.y;
+			myImage[25] = game.add.sprite(x, y, 'water');
+			myImage[25].height = sizeTiles;
+			myImage[25].width = sizeTiles;
+			this.tileGrid[5][0] = 7;
+
+			var x = myImage[44].position.x;
+			var y = myImage[44].position.y;
+			myImage[44] = game.add.sprite(x, y, 'water');
+			myImage[44].height = sizeTiles;
+			myImage[44].width = sizeTiles;
+			this.tileGrid[8][4] = 7;
+		} else if (myImage[2].key === 'water') {
+			var x = myImage[2].position.x;
+			var y = myImage[2].position.y;
+			myImage[2] = game.add.sprite(x, y, 'earth');
+			myImage[2].height = sizeTiles;
+			myImage[2].width = sizeTiles;
+			this.tileGrid[0][2] = 8;
+
+			var x = myImage[25].position.x;
+			var y = myImage[25].position.y;
+			myImage[25] = game.add.sprite(x, y, 'earth');
+			myImage[25].height = sizeTiles;
+			myImage[25].width = sizeTiles;
+			this.tileGrid[5][0] = 8;
+
+			var x = myImage[44].position.x;
+			var y = myImage[44].position.y;
+			myImage[44] = game.add.sprite(x, y, 'earth');
+			myImage[44].height = sizeTiles;
+			myImage[44].width = sizeTiles;
+			this.tileGrid[8][4] = 8;
+		} else if (myImage[2].key === 'earth') {
+			var x = myImage[2].position.x;
+			var y = myImage[2].position.y;
+			myImage[2] = game.add.sprite(x, y, 'fire');
+			myImage[2].height = sizeTiles;
+			myImage[2].width = sizeTiles;
+			this.tileGrid[0][2] = 5;
+
+			var x = myImage[25].position.x;
+			var y = myImage[25].position.y;
+			myImage[25] = game.add.sprite(x, y, 'fire');
+			myImage[25].height = sizeTiles;
+			myImage[25].width = sizeTiles;
+			this.tileGrid[5][0] = 5;
+
+			var x = myImage[44].position.x;
+			var y = myImage[44].position.y;
+			myImage[44] = game.add.sprite(x, y, 'fire');
+			myImage[44].height = sizeTiles;
+			myImage[44].width = sizeTiles;
+			this.tileGrid[8][4] = 5;
+		}
+		console.log('this.tileGrid: ', this.tileGrid);
+	},
+	update: function() {
+		if (this.spaceKey.isDown) {
+			while (!gameOver) {
+				line = game.add.graphics(0, 0);
+				line.lineStyle(5, '#ffff00', 1);
+				line.moveTo(positionLine[0], positionLine[1]);
+				this.increaseLine();
+				line.lineTo(positionLine[0], positionLine[1]);
+			}
+		}
+	},
+	rotateAir: function() {
+		if (vent.direction === 'right') {
+			vent.direction = 'down';
+			this.air.angle = 90;
+		} else if (vent.direction === 'down') {
+			this.air.angle = 180;
+			vent.direction = 'left';
+		} else if (vent.direction === 'left') {
+			this.air.angle = 270;
+			vent.direction = 'up';
+		} else if (vent.direction === 'up') {
+			this.air.angle = 0;
+			vent.direction = 'right';
+		}
+	},
 	initGrid: function(tileGrid) {
 		let count = -1;
 		for (let i = 0; i < tileGrid.length; i++) {
@@ -178,6 +329,62 @@ var gameState = {
 				if (tileGrid[i][j] === 0) game.physics.arcade.enable(myImage[count]);
 			}
 		}
+	},
+	checkCollision: function() {
+		console.log('lineTab: ', lineTab);
+		console.log('columnTab: ', columnTab);
+		if (lineTab < 0 || columnTab > 4 || columnTab < 0 || lineTab > 10)
+			gameOver = true;
+		if (this.tileGrid[lineTab][columnTab] === 1) gameOver = true;
+	},
+	checkElement: function() {
+		var y = lineTab;
+		var x = columnTab;
+
+		if (this.tileGrid[y][x] === 4) {
+			//elem fire
+		}
+		if (this.tileGrid[y][x] === 5) {
+			//elem air
+			lineDirection = vent.direction;
+		}
+		if (this.tileGrid[y][x] === 6) {
+			//elem water
+		}
+		if (this.tileGrid[y][x] === 7) {
+			//elem eart
+		}
+	},
+	increaseLine: function() {
+		this.checkCollision();
+		this.checkElement();
+		if (!gameOver) {
+			if (lineDirection === 'up') {
+				this.incrementUp();
+			} else if (lineDirection === 'right') {
+				this.incrementRight();
+			} else if (lineDirection === 'down') {
+				this.incrementDown();
+			} else if (lineDirection === 'left') {
+				this.incrementLeft();
+			}
+		}
+	},
+	incrementUp: function() {
+		lineTab -= 1;
+		positionLine[1] -= 50;
+	},
+	incrementLeft: function() {
+		columnTab -= 1;
+		positionLine[0] -= 50;
+	},
+	incrementRight: function() {
+		columnTab += 1;
+		positionLine[0] += 50;
+	},
+	incrementDown() {
+		lineTab += 1;
+		positionLine[1] += 50;
 	},
 };
 
